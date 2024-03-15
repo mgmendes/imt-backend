0f9ab2a73c6c4e14684ca944549cfb52495549ba
Marcio Mendes <mgmendes6@gmail.com>
2024-03-14 22:25:21 -0300

    jest test implementado

--- calc.test.js
+++ calc.test.js
@@ -1,4 +1,4 @@
-const { soma, multiplicacao, subtracao } = require("./calc");
+const { soma, multiplicacao, subtracao, divisao } = require("./calc");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(soma(1, 2)).toBe(3);
@@ -10,4 +10,8 @@ test("subtract 6 - 2 to equal 4", () => {
 
 test("multiply 4 * 5 to equal 20", () => {
   expect(multiplicacao(4, 5)).toBe(20);
+});
+
+test("divide 10 / 2 to equal 5", () => {
+  expect(divisao(10, 2)).toBe(5);
 });
