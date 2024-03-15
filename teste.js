42a71cf294731de82446d7e76022d8d9ff68ab70
Marcio Mendes <mgmendes6@gmail.com>
2024-03-14 21:48:39 -0300

    apostila02(js-git): teste da funcao divisao implementada

--- teste.js
+++ teste.js
@@ -5,3 +5,5 @@ console.log("soma(2,3) =", calc.soma(2, 3));
 console.log("subtracao(5,2) =", calc.subtracao(5, 2));
 
 console.log("multiplicacao(3,4) = ", calc.multiplicacao(3, 4));
+
+console.log("divisao(10,2) = ", calc.divisao(10, 2));
