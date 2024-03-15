d116cb1c560352bb14034e1b705cc5f14e45dc27
Marcio Mendes <mgmendes6@gmail.com>
2024-03-14 21:47:12 -0300

    apostila02(git-js): teste multiplicar implementado

--- teste.js
+++ teste.js
@@ -3,3 +3,5 @@ const calc = require("./calc");
 console.log("soma(2,3) =", calc.soma(2, 3));
 
 console.log("subtracao(5,2) =", calc.subtracao(5, 2));
+
+console.log("multiplicacao(3,4) = ", calc.multiplicacao(3, 4));
